3da3d4c6856f8189987d74c062689069db5d5209
lukas-pilka <pilka@blueghost.cz>
2023-05-08 11:14:25 +0200

    Přidává fitraci v položkách CV

--- client/src/App.js
+++ client/src/App.js
@@ -6,23 +6,25 @@ import {format} from 'date-fns';
 
 export function CvEvents() {
     const [cvEvents, setCvEvents] = React.useState(null);
+    const [cvTypes, setCvTypes] = React.useState(null);
+    const [selectedCvType, setSelectedCvType] = React.useState("All");
 
     React.useEffect(() => {
         fetch("/results")
             .then((res) => res.json())
-            .then(data => {
-                const modifiedData = data.map(cvEvent => {
+            .then(rawCvEvents => {
+                const structuredCvEvents = rawCvEvents.map(cvEvent => {
                     const event = {}
                     event.name = cvEvent._source.eventName;
                     event.startDate = new Date(cvEvent._source.eventStartDate);
                     event.oneDayEvent = cvEvent._source.oneDayEvent;
-                    if (cvEvent._source.eventEndDate){
+                    if (cvEvent._source.eventEndDate) {
                         event.endDate = new Date(cvEvent._source.eventEndDate);
                         event.inProgress = false;
-                    }else if(!cvEvent._source.eventEndDate && event.oneDayEvent === true){
+                    } else if (!cvEvent._source.eventEndDate && event.oneDayEvent === true) {
                         event.endDate = event.startDate;
                         event.inProgress = false;
-                    }else{
+                    } else {
                         event.endDate = new Date();
                         event.inProgress = true;
                     }
@@ -32,29 +34,62 @@ export function CvEvents() {
                     return event
                 });
                 // sorting events by date
-                modifiedData.sort((a, b) => (a.endDate < b.endDate) ? 1 : -1);
-                setCvEvents(modifiedData);
+                structuredCvEvents.sort((a, b) => (a.endDate < b.endDate) ? 1 : -1);
+
+                // filtering by selectedCvType
+                if(selectedCvType === "All"){
+                    setCvEvents(structuredCvEvents);
+                }else{
+                    const filteredData = structuredCvEvents.filter(obj => obj.type === selectedCvType);
+                    setCvEvents(filteredData);
+                }
+
+                // setting categories
+                const cvTypesArray = Array.from(new Set(structuredCvEvents.map(event => event.type)));
+                setCvTypes(cvTypesArray);
+
             }, []);
     });
 
+    const handleTypeClick = (selectedType) => {
+        setSelectedCvType(selectedType)
+    };
+
     return (
         <div>
-            {/*{JSON.stringify(cvEvents)}*/}
-            ┌{format(new Date(),'dd/MM/yyyy HH:mm:ss')}<br/>
+            <section>
+            {/* Category menu */}
+            <nav>
+            Fiter:&nbsp;
+            {!cvTypes ? "Loading..." :
+                <span>
+                    <button href="#" onClick={() => handleTypeClick("All")} className={selectedCvType === "All" ? "active" : ""}>All</button>
+                    {cvTypes.map((type, index) => (
+                        <button key={index} onClick={() => handleTypeClick(type)} className={selectedCvType === type ? "active" : ""}>{type}</button>
+                    ))}
+                </span>
+            }
+            </nav>
+            <p>↓↓↓</p>
+
+            {/*CV events list */}
+            ┌{format(new Date(), 'dd/MM/yyyy HH:mm:ss')}<br/>
             │<br/>
             {!cvEvents ? "Loading..." :
                 cvEvents.map((e, index) => (
-                    <div key={index}>
+                    <div key={index} className="cvEvent">
                         ├{!e.url ? e.name : <a href={e.url} target="_blank" rel="noreferrer">{e.name}</a>}<br/>
                         │{e.type} | {e.description}<br/>
                         │{e.inProgress ? "since " : ""}{format(e.startDate, 'MM/yyyy')}
-                        {e.endDate !== e.startDate && e.inProgress === false ? " - " + format(e.endDate, 'MM/yyyy'): ""}<br/>
+                        {e.endDate !== e.startDate && e.inProgress === false ? " - " + format(e.endDate, 'MM/yyyy') : ""}<br/>
 
                         │
                     </div>
                 ))
             }
             └13/08/1986
+
+            </section>
         </div>
     );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -20,21 +20,25 @@ root.render(
             <link href="https://fonts.googleapis.com/css2?family=Ubuntu+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
             <script async src="https://www.googletagmanager.com/gtag/js?id=UA-144703876-4"></script>
         </Helmet>
-        <h1>Lukas Pilka</h1>
-        <p>===========</p>
+        <section>
+            <h1>Lukas Pilka</h1>
+        </section>
+        <section>
         <p className="profileText">Lukas Pilka is a digital designer, innovator and media theorist focusing on interactive and communication design, contemporary technologies, new media and the overlap between these fields and the world of fine art. His <a href="https://drive.google.com/file/d/1bIFjaWXvsRleSBSBGwtUjeANgOoTEW8D/view?usp=share_link" target="_blank" rel="noopener noreferrer">recently completed PhD</a> at the Academy of Arts, Architecture and Design in Prague focused on the use of computer vision and neural networks for automated classification and quantitative interpretation of artworks. Lukas Pilka is also a co-founder of the software company <a href="https://blueghost.cz" target="_blank" rel="noopener noreferrer">BlueGhost</a>, a university lecturer and a contributor to many media outlets. He is the initiator of the <a href="https://otevrenesbirky.cz/" target="_blank" rel="noopener noreferrer">Open Collections project</a>, creator of the experimental application <a href="https://digitalcurator.art" rel="noopener noreferrer" target="_blank">DigitalCurator.art</a>, programmer, podcaster and enthusiast of old and new generative art.</p>
         <p>
             Lives in Prague, Czech Republic, EU<br/>
             Cell: +420 774 502 521<br/>
             hello.lukas.pilka@gmail.com<br/>
-
-            <a href="https://www.linkedin.com/in/lukas-pilka/" target="_blank" rel="noopener noreferrer">in</a> &nbsp;
-            <a href="https://twitter.com/LukasPilka" target="_blank" rel="noopener noreferrer">Tw</a> &nbsp;
-            <a href="https://podcasts.apple.com/us/podcast/blueghost-update/id1539124027" target="_blank" rel="noopener noreferrer">Apple Podcasts</a> &nbsp;
-            <a href="https://open.spotify.com/show/5DTkvyEUnUvR7yBqxqqj1V" target="_blank" rel="noopener noreferrer">Spotify</a> &nbsp;
-            <a href="https://photos.app.goo.gl/TmbHmNyrTCCmDxcW9" target="_blank" rel="noopener noreferrer">Profile photos</a> &nbsp;
         </p>
-        <p>===========</p>
+        <nav>
+            Social & resources:&nbsp;
+                <a href="https://www.linkedin.com/in/lukas-pilka/" target="_blank" rel="noopener noreferrer">in</a>
+                <a href="https://twitter.com/LukasPilka" target="_blank" rel="noopener noreferrer">Tw</a>
+                <a href="https://podcasts.apple.com/us/podcast/blueghost-update/id1539124027" target="_blank" rel="noopener noreferrer">Apple Podcasts</a>
+                <a href="https://open.spotify.com/show/5DTkvyEUnUvR7yBqxqqj1V" target="_blank" rel="noopener noreferrer">Spotify</a>
+                <a href="https://photos.app.goo.gl/TmbHmNyrTCCmDxcW9" target="_blank" rel="noopener noreferrer">Profile photos</a>
+            </nav>
+        </section>
         <CvEvents />
     </React.StrictMode>
 );
